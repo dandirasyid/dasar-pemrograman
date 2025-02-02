21bebf5b8e885a563bc5cc8da8ce5cdd7efdd31c
dandirasyid <dandirasyid2003@gmail.com>
2025-02-03 02:23:08 +0700

    modified: belajar html

--- js-freecodecamp/pyramid/script.js
+++ js-freecodecamp/pyramid/script.js
@@ -1,55 +1,55 @@
-// const character = "#";
-// const count = 8;
-// const rows = [];
-// let inverted = true;
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
 
-// function padRow(rowNumber, rowCount) {
-//     return (
-//         " ".repeat(rowCount - rowNumber) +
-//         character.repeat(2 * rowNumber - 1) +
-//         " ".repeat(rowCount - rowNumber) +
-//         " ".repeat(rowCount - rowNumber) +
-//         " " +
-//         character.repeat(2 * rowNumber - 1) +
-//         " ".repeat(rowCount - rowNumber)
-//     );
-// }
+function padRow(rowNumber, rowCount) {
+    return (
+        " ".repeat(rowCount - rowNumber) +
+        character.repeat(2 * rowNumber - 1) +
+        " ".repeat(rowCount - rowNumber) +
+        " ".repeat(rowCount - rowNumber) +
+        " " +
+        character.repeat(2 * rowNumber - 1) +
+        " ".repeat(rowCount - rowNumber)
+    );
+}
 
-// function padRowReverse(rowNumber, rowCount) {
-//     return (
-//         " ".repeat(rowCount - rowNumber) +
-//         character.repeat(2 * rowNumber - 1) +
-//         " ".repeat(rowCount - rowNumber) +
-//         " ".repeat(rowCount - rowNumber) +
-//         " " +
-//         character.repeat(2 * rowNumber - 1) +
-//         " ".repeat(rowCount - rowNumber)
-//     );
-// }
+function padRowReverse(rowNumber, rowCount) {
+    return (
+        " ".repeat(rowCount - rowNumber) +
+        character.repeat(2 * rowNumber - 1) +
+        " ".repeat(rowCount - rowNumber) +
+        " ".repeat(rowCount - rowNumber) +
+        " " +
+        character.repeat(2 * rowNumber - 1) +
+        " ".repeat(rowCount - rowNumber)
+    );
+}
 
-// for (let i = 1; i <= count; i++) {
-//     if (inverted) {
-//         rows.unshift(padRow(i, count));
-//     } else {
-//         rows.push(padRow(i, count));
-//     }
-// }
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    } else {
+        rows.push(padRow(i, count));
+    }
+}
 
-// for (let i = 1; i <= count; i++) {
-//     if (!inverted) {
-//         rows.unshift(padRowReverse(i, count));
-//     } else {
-//         rows.push(padRowReverse(i, count));
-//     }
-// }
+for (let i = 1; i <= count; i++) {
+    if (!inverted) {
+        rows.unshift(padRowReverse(i, count));
+    } else {
+        rows.push(padRowReverse(i, count));
+    }
+}
 
-// let result = "";
+let result = "";
 
-// for (const row of rows) {
-//     result = result + row + "\n";
-// }
+for (const row of rows) {
+    result = result + row + "\n";
+}
 
-// console.log(result);
+console.log(result);
 
 // Cara lain membuat segitiga
 function segitiga(rowCount) {
